bcc70d6937e8fcc7ffa3693ed202655944831353
MuhammadShidqulIman <muhammadshidqul3@gmail.com>
2025-11-27 11:36:40 +0700

    Setup AppBar siswa

---
+++ app/src/main/java/com/example/myroomsatu/view/AppBar.kt
@@ -0,0 +1,15 @@
+package com.example.myroomsatu.view
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.TopAppBarScrollBehavior
+import androidx.compose.runtime.Composable
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SiswaTopAppBar(
+    title: String,
+    canNavigateBack: Boolean,
+    modifier: Modifier = Modifier,
+    scrollBehavior: TopAppBarScrollBehavior? = null,
+    navigateUp: () -> Unit = {}
+) {
