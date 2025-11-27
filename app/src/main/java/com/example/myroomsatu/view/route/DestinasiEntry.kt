15af72a8123bc9c1a9e210628af36cec255c4b8d
MuhammadShidqulIman <muhammadshidqul3@gmail.com>
2025-11-27 11:26:28 +0700

    Menambah objek destinasi dan interface destinasi navigasi

---
+++ app/src/main/java/com/example/myroomsatu/view/route/DestinasiEntry.kt
@@ -0,0 +1,8 @@
+package com.example.myroomsatuview.route
+
+import com.example.myroomsatu.view.route.DestinasiNavigasi
+
+object DestinasiEntry: DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = "Tambah Siswa"
+}
