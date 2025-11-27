c60df94053355793cbbf7634064fa4fb3ef6db07
MuhammadShidqulIman <muhammadshidqul3@gmail.com>
2025-11-27 11:39:07 +0700

    update appbar

--- app/src/main/java/com/example/myroomsatu/view/AppBar.kt
+++ app/src/main/java/com/example/myroomsatu/view/AppBar.kt
@@ -1,11 +1,15 @@
 package com.example.myroomsatu.view
 
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -32,5 +36,4 @@ fun SiswaTopAppBar(
             }
         }
     )
-}
 }
