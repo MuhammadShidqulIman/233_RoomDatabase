360657532179f853644cd7bba5f21fe353748b88
MuhammadShidqulIman <muhammadshidqul3@gmail.com>
2025-12-10 16:03:32 +0700

    Menambah initializer editviewmodel

--- app/src/main/java/com/example/myroomsatu/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/myroomsatu/viewmodel/provider/PenyediaViewModel.kt
@@ -1,13 +1,12 @@
 package com.example.myroomsatu.viewmodel.provider
-
-
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.myroomsatu.ContainerApp
-import com.example.myroomsatu.view.viewmodel.DetailViewModel
+import com.example.myroomsatu.viewmodel.DetailViewModel
+import com.example.myroomsatu.viewmodel.EditViewModel
 import com.example.myroomsatu.viewmodel.EntryViewModel
 import com.example.myroomsatu.viewmodel.HomeViewModel
 
@@ -15,29 +14,31 @@ object PenyediaViewModel {
     val Factory = viewModelFactory {
 
         initializer {
-
             HomeViewModel(
-
                 aplikasi().container.repositoriSiswa
             )
         }
+
         initializer {
             DetailViewModel(
                 savedStateHandle = this.createSavedStateHandle(),
-                repositoriSiswa = ContainerApp().container.repositoriSiswa
+                repositoriSiswa = aplikasi().container.repositoriSiswa
             )
         }
 
         initializer {
-
             EntryViewModel(
-
                 aplikasi().container.repositoriSiswa
+            )
+        }
+
+        initializer {
+            EditViewModel(
+                savedStateHandle = this.createSavedStateHandle(),
+                repositoriSiswa = aplikasi().container.repositoriSiswa
             )
         }
     }
 }
-
 fun CreationExtras.aplikasi(): ContainerApp =
-
     (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as ContainerApp)
